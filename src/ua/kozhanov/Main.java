ba934561ba6fab09432ee95cda15416f3ad19b80
Grandius <gradius777@gmail.com>
2021-06-20 20:00:25 +0300

    Initial commit of HW15

---
+++ src/ua/kozhanov/Main.java
@@ -0,0 +1,26 @@
+package ua.kozhanov;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        TreeSet<String> treeSet = new TreeSet<>();
+        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        arrayList.add("ArListElement1");
+        arrayList.add("ArListElement2");
+        arrayList.add("ArListElement3");
+        arrayList.add("ArListElement4");
+        arrayList.add("ArListElement5");
+        arrayList.add("ArListElement6");
+
+        System.out.println("ArrayList elements: "+arrayList);
+
+
+    }
+}
